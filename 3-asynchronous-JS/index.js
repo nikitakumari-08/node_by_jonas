9d25740ab68b6a9202188666df72495038d39d26
Nikita Kumari <nikitarai7305@gmail.com>
2025-10-10 19:31:19 +0530

    ok4

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -15,12 +15,6 @@ const readFilePro = file => {
 
 
 
-const writeFilePro = (file, data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(file, data, err => {
-            if (err) reject ('Could not write file')
-                resolve ('success');
-        });
 
     });
 
